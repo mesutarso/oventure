086c886001a1374b7890daab8bfecb17ee7efab8
mechack tambwe <mesutarso@users.noreply.github.com>
2024-07-22 11:45:55 +0100

    fix some issues

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -135,7 +135,7 @@ const FormDescription = React.forwardRef<
     <p
       ref={ref}
       id={formDescriptionId}
-      className={cn("text-sm text-zinc-500 dark:text-zinc-400", className)}
+      className={cn("text-sm text-neutral-500 dark:text-neutral-400", className)}
       {...props}
     />
   )
