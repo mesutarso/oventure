12bf215822b94f83ac6bfe399a129a7bfe9788f1
mechack tambwe <mesutarso@users.noreply.github.com>
2024-07-23 04:23:21 +0100

    fix some issues

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -6,16 +6,6 @@ import { Button } from "../ui/button";
 function Hero() {
   return (
     <div className="relative h-screen overflow-hidden">
-      {/* <Video
-        src={'/videos/forest.mp4'}
-        autoPlay
-        controls={false}
-        loop
-        muted
-        playsInline
-        className={" top-0 left-0 w-full h-full object-cover"}
-      /> */}
-      {/* <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-black bg-second md:bg-transparent "></div> */}
       <Image
         src={"/images/oventure-cover.png"}
         width={1920}
@@ -26,7 +16,7 @@ function Hero() {
       <div className="relative z-10 flex flex-col items-start justify-center h-full text-white space-y-8 container">
         <h1 className="text-2xl font-black leading-tight sm:text-3xl md:text-4xl lg:text-5xl lg:w-3/6 uppercase">
           Catalyseur d&apos;innovation pour un monde
-          <span className="text-secondary">durable.</span>
+          <span className="text-secondary"> durable.</span>
         </h1>
         <p className="mt-4 text-xs sm:text-sm lg:text-base font-light lg:w-3/6">
           Investissements durables en RDC et compensation carbone dans les pays
--- components/home/office/index.tsx
+++ components/home/office/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import HorizontalCarousel from "@/components/home/office/horizontal-carousel";
 
 function Office() {
   return (
@@ -15,96 +15,9 @@ function Office() {
           des décideurs publiques.
         </p>
       </div>
-      <div
-        className="relative w-full pb-20 hidden md:block"
-        role="region"
-        aria-roledescription="carousel"
-      >
-        <div className="overflow-hidden">
-          <div className="flex -ml-1">
-            <div
-              role="group"
-              aria-roledescription="slide"
-              className="min-w-0 shrink-0 grow-0 basis-full pl-1 md:basis-2/3 lg:basis-2/5"
-            >
-              <div className="p-1">
-                <div className="rounded-xl border text-card-foreground bg-primary shadow-sm border-none w-full">
-                  <div className="flex aspect-[16/12] items-center justify-center p-0 relative">
-                    <h1 className="absolute bottom-0 left-0 p-10 text-3xl md:text-4xl lg:text-5xl font-black uppercase outlined-text text-white">
-                      grands projets big impact.
-                    </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div
-              role="group"
-              aria-roledescription="slide"
-              className="min-w-0 shrink-0 grow-0 basis-full pl-1 md:basis-2/3 lg:basis-2/5"
-            >
-              <div className="p-1">
-                <div className="rounded-xl border text-card-foreground bg-secondary shadow-sm border-none w-full">
-                  <div className="flex aspect-[16/12] items-center justify-center p-0 relative">
-                    <h1 className="absolute bottom-0 left-0 p-10 text-3xl md:text-4xl lg:text-5xl font-black uppercase outlined-text">
-                      le plus grand marché carbone en afrique
-                    </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div
-              role="group"
-              aria-roledescription="slide"
-              className="min-w-0 shrink-0 grow-0 basis-full pl-1 md:basis-2/3 lg:basis-2/5"
-            >
-              <div className="p-1">
-                <div className="rounded-xl border text-card-foreground bg-blueLight shadow-sm border-none w-full">
-                  <div className="flex aspect-[16/12] items-center justify-center p-0 relative">
-                    <h1 className="absolute bottom-0 left-0 p-10 text-3xl md:text-4xl lg:text-5xl font-black uppercase outlined-text text-white">
-                      analyses &amp; rapports
-                    </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="min-h-screen flex md:hidden flex-col items-start justify-start space-y-8 overflow-hidden">
-        <div className="relative w-full h-full pb-72">
-          <div className="w-11/12 absolute left-4">
-            <div className="rounded-xl border text-card-foreground bg-main shadow-sm border-none w-full">
-              <div className="flex aspect-[16/12] items-center justify-center p-0 relative">
-                <h1 className="absolute bottom-0 left-0 p-10 text-3xl md:text-4xl lg:text-5xl font-black uppercase outlined-text">
-                  grands projets big impact.
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="relative w-full h-full pb-72">
-          <div className="w-11/12 absolute left-6">
-            <div className="rounded-xl border text-card-foreground bg-third shadow-sm border-none w-full">
-              <div className="flex aspect-[16/12] items-center justify-center p-0 relative">
-                <h1 className="absolute bottom-0 left-0 p-10 text-3xl md:text-4xl lg:text-5xl font-black uppercase outlined-text">
-                  le plus grand marché carbone en afrique
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="relative w-full h-full pb-72">
-          <div className="w-11/12 absolute left-[35vw]">
-            <div className="rounded-xl border text-card-foreground bg-sky-300 shadow-sm border-none w-full">
-              <div className="flex aspect-[16/12] items-center justify-center p-0 relative">
-                <h1 className="absolute bottom-0 left-0 p-10 text-3xl md:text-4xl lg:text-5xl font-black uppercase outlined-text">
-                  analyses &amp; rapports
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+
+      <HorizontalCarousel />
+
     </div>
   );
 }
