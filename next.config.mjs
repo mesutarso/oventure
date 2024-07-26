37760b1334dd6d9e660ea4470f464f9ca53a182d
mechack tambwe <mesutarso@users.noreply.github.com>
2024-07-26 18:56:29 +0100

    ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'oventure.mechacktambwe.me',
-
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "oventure.mechacktambwe.me",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextVideo(nextConfig);
