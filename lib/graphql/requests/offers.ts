f5edd08a20c7541c5097739622f9b3845c2d652e
mechack tambwe <mesutarso@users.noreply.github.com>
2025-03-20 12:27:00 +0100

    fix issues

--- lib/graphql/requests/offers.ts
+++
@@ -1,7 +0,0 @@
-import  {fetchGraphql} from "@/lib/graphql/init";
-import {GET_OFFERS} from "@/lib/graphql/queries";
-
-export async function getOffers() {
-  const data = await fetchGraphql(GET_OFFERS);
-  return data.offres.edges.map(({ node }:any) => node);
-}
